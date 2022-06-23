d7d7063a7674baaf7cc840f45fd4862e3d87a962
Nero-Link <nerolink1107@gmail.com>
2022-06-23 17:02:48 +1000

    auth

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,51 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBiUssFRmOVuKHl3q-BLFUmVlawCu599WM",
+  authDomain: "crown-db-3d028.firebaseapp.com",
+  projectId: "crown-db-3d028",
+  storageBucket: "crown-db-3d028.appspot.com",
+  messagingSenderId: "430242297771",
+  appId: "1:430242297771:web:82746ada9498283a0484ee",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("Error creating the user", error.message);
+    }
+  }
+  return userDocRef;
+};
