f831dfa7c4a4168e74bc257b97ebbde7c0609ef7
Nero-Link <nerolink1107@gmail.com>
2022-06-30 15:53:56 +1000

    typed store + middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
+import { compose, createStore, applyMiddleware, Middleware } from "redux";
+import { persistStore, persistReducer, PersistConfig } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import { loggerMiddleware } from "./middleware/logger";
@@ -7,10 +7,21 @@ import { loggerMiddleware } from "./middleware/logger";
 //SAGA
 import createSagaMiddleware from "redux-saga";
 import { rootSaga } from "./root-saga";
-
 import { rootReducer } from "./root-reducer";
 
-const persistConfig = {
+export type RootState = ReturnType<typeof rootReducer>;
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[];
+};
+
+const persistConfig: ExtendedPersistConfig = {
   key: "root",
   storage,
   whitelist: ["cart"],
@@ -25,7 +36,7 @@ const middleWares = [
   // thunk,
   //SAGA
   sagaMiddleware,
-].filter(Boolean);
+].filter((middleware): middleware is Middleware => Boolean(middleware));
 // const middleWares = [loggerMiddleware];
 
 const composeEnhancer =
